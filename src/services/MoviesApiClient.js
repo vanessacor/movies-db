7fb8d9fa49ffe5581ba22cce615c4fcffa12e425
vanessa <vanessapcbarros@gmail.com>
2020-12-09 10:06:07 +0100

    clean comments

--- src/services/MoviesApiClient.js
+++ src/services/MoviesApiClient.js
@@ -1,43 +1,41 @@
 import http from "./httpClient";
 
 class MoviesApiClient {
-  
   getAllMovies() {
-    return http.get("/movies")
+    return http.get("/movies");
   }
 
   getMovie(id) {
     return http.get(`movies/${id}`);
   }
 
-  // deleteMovie(id) {
-  //   return get(`${this.baseUrl}/books/${id}`, {
-  //     method: "delete",
-  //   });
-  // }
+  deleteMovie(id) {
+    return get(`/movies/${id}`, {
+      method: "delete",
+    });
+  }
 
-  // createMovie(movie) {
-  //   return get(`${this.baseUrl}/movies`, {
-  //     method: "POST",
-  //     body: JSON.stringify(movie),
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  // }
+  createMovie(movie) {
+    return get("/movies", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-  // updateMovie(movie) {
-  //   return get(`${this.baseUrl}/movies/${movie.id}`, {
-  //     method: "PUT",
-  //     body: JSON.stringify(movie),
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  // }
+  updateMovie(movie) {
+    return get(`/movies/${movie.id}`, {
+      method: "PUT",
+      body: JSON.stringify(movie),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
 
 export default new MoviesApiClient();
-
