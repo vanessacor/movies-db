6d58dfe2d53f862bf985907c4705d15411b156e5
vanessa <vanessapcbarros@gmail.com>
2021-06-02 18:34:45 +0200

    test: add test for inputText component

---
+++ tests/unit/inputText.spec.js
@@ -0,0 +1,25 @@
+import { mount } from "@vue/test-utils";
+import InputText from "@/components/InputText";
+
+describe("test InputText.vue", () => {
+  let wrapper = null;
+  const value = "Vanessa trip";
+  const label = "Insert the title here";
+  beforeEach(() => {
+    wrapper = mount(InputText, {
+      propsData: { value, label },
+    });
+  });
+  afterEach(() => {
+    wrapper.destroy();
+  });
+  it("renders props when passed", () => {
+    expect(wrapper.vm.value).toMatch(value);
+    expect(wrapper.vm.label).toMatch(label);
+  });
+
+  it("emits an event", () => {
+    wrapper.vm.$emit("input");
+    expect(wrapper.emitted().input).toBeTruthy();
+  });
+});
